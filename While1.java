4bda2c3e37730ccc9eb90ac4b0d3a880b5017d26
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 07:53:34 -0300

    Create While1.java

---
+++ While1.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class While1 {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i);
+			i = i + i;
+			i++;
+		}
+	}
+}

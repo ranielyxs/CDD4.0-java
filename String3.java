4eb3110b2801a7c7a10060513ebb39fb5f27a83b
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 09:24:17 -0300

    Create String3.java

---
+++ String3.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class String3 {
+	public static void main(String[] args) {
+		
+		String s = "ABC";
+		int tam = s.length();
+		//retorna o 3 já que a string s possui 3 caracteres.
+		
+		System.out.println(s);
+
+
+	}
+
+
+}

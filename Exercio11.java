daf54b4064427d94ef169b989b3b4317df8af8c8
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 07:51:09 -0300

    Create Exercio11.java

---
+++ Exercio11.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Exercio11 {
+
+	public static void main(String[]args) { 
+		Scanner palavra= new Scanner(System.in);
+		System.out.println("Digite suas notas:");
+		char letra = palavra.next().charAt(0);
+		
+		if (letra = 'F' || letra <= 'M') {
+		  System.out.println('invalido');
+		}
+		else if ( letra = 'f') {
+			}
+		}
+
+}

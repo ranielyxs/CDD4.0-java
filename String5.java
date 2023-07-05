bd64049ffd58406be1085c2c0b7d9267b27f7cdc
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 11:14:33 -0300

    Create String5.java

---
+++ String5.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class String5 {
+	public static void main(String[] args) {
+		
+		String str = "Hello World World2";
+		int pos = str.lastIndexOf("l");
+		
+		//retorna o indice da ultima ocorrencia de "l"
+		
+		System.out.println(pos);
+
+	}
+}

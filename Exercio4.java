c8a26c5540ce2303256f886da243401c4e700123
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 07:38:56 -0300

    Create Exercio4.java

---
+++ Exercio4.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Exercio4 {
+		 public static void main(String[]args) { 
+			int a = 3;
+			int b = 4;
+			int c = 7;
+			if(a++>=b)
+				System.out.println(--c);
+			else
+				System.out.println(c++);
+			}
+}
+

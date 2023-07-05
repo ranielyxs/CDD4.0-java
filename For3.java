b11925bb87b6a68f42344520daf16876eb64fdd5
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 08:01:20 -0300

    Create For3.java

---
+++ For3.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class For3 {
+	public static void main(String[] args) {
+		for(int i = 0; i <= 10; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(j + ".");
+				}
+			System.out.println("");
+			}
+
+	}
+
+}

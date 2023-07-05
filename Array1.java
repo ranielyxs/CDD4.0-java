65fe23e88e4232a744fe5a4f95ffa4d4c2016aab
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 08:04:20 -0300

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Array1 {
+	public static void main(String[] args) {
+		int[] intArray= {2,3,4,5,6,7,8,9};
+		
+		for(int i=0; i<intArray.length; i++) {
+			System.out.print(intArray[i]);
+		}
+	}
+
+}

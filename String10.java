60f2059e49b051dc7cf0ebc21a81f8c40799a83d
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 11:08:55 -0300

    Create String10.java

---
+++ String10.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class String10 {
+	public static void main(String[] args) {
+		
+		String t1 = "Iguais";
+		String t2 = "Iguais";
+		boolean b2 = t1.equals(t2);
+		
+		System.out.println(b2);
+	}
+
+}

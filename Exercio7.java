d2a685382c396f3b4beb2782a5ce68ce689f40f7
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 07:44:06 -0300

    Create Exercio7.java

---
+++ Exercio7.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Exercio7 {		
+	public static void main(String[]args) { 
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite um número:");
+		double resposta = entrada.nextDouble();
+		System.out.println(resposta);
+		
+}
+
+
+}

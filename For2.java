fa149c616d4d5fd43eb8c8b32ae109b272903b87
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 08:00:43 -0300

    Create For2.java

---
+++ For2.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class For2 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			if (i > 50 && i < 60) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+
+}

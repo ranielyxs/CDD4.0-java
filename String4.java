1a6d8cd09e2ab1faa9ccde72080f6a397d6efd59
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 11:13:04 -0300

    Create String4.java

---
+++ String4.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class String4{
+	public static void main(String[] args) {
+		String str = "Hello World World2";
+		int pos = str.indexOf("l");
+		
+		//retorna o indice da primeira ocorrencia de "l"
+	
+		System.out.println(pos);
+
+	}
+}

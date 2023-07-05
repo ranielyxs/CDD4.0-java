dbabcd86493f6fae8d4326e3c747215de28fbd6d
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 08:00:07 -0300

    Create For1.java

---
+++ For1.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class For1 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			if (i == 20) {
+			System.out.println("Achei");
+			break;
+		}
+			System.out.println(i);
+		}
+
+	}
+
+}

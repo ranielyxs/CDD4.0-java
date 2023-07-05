c6256c523be1c30885f1e3b299cd6e1dd84a1930
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 07:42:20 -0300

    Create Exercio5.java

---
+++ Exercio5.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Exercio5 {
+	public static void main(String[]args) {
+		int idade = 15;
+		if(idade<18) {
+			System.out.println("Não pode entrar");
+		}else {
+			System.out.println("Pode entrar");
+		}
+	}
+}

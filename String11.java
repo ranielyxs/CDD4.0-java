fb6d391c865ac8b9a8930d8a1bb580a44d02d7cd
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-05 11:26:43 -0300

    Create String11.java

---
+++ String11.java
@@ -0,0 +1,17 @@
+package fundamentos;
+import java.util.Scanner;
+public class String11 {
+	public static void main(String[] args) {
+		
+Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite uma Frase:");
+        String frase = entrada.nextLine();
+        
+        String resposta = entrada.nextLine();
+		System.out.println(frase.toUpperCase());
+		
+	}
+
+
+}

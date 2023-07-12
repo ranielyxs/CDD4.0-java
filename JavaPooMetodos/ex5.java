6f76f9eda9b11ef6bc1ee889362fca48743fcc1b
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 11:16:13 -0300

    Create ex5.java

---
+++ JavaPooMetodos/ex5.java
@@ -0,0 +1,12 @@
+package JavaPooMetodos;
+
+public class exMedia {
+	public static void main (String[] args) {
+		SomarMetodos notas = new SomarMetodos();
+        int media = notas.media(2, 2, 2);
+		
+		System.out.printf("sua media é %d",media);
+	}
+
+
+}

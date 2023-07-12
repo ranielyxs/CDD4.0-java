300884b697aec50d6e1c696b6fae302aa705fa56
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 11:14:46 -0300

    Update ex3.java

--- JavaPooMetodos/ex3.java
+++ JavaPooMetodos/ex3.java
@@ -4,7 +4,11 @@ public class SomarMetodos {
 	int somar (int a,int b) {
 		return a + b;
 	}
-    int  somar (int a,int b,int c) {
+        int  somar (int a,int b,int c) {
     	return a + b + c;
     }
+	int media (int a,int b,int c) {
+    		return (a + b + c)/3;
+    }
 }
+

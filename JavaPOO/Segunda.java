4b39da6b539c7425277c1a2e20d24f6312cf022c
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 09:04:44 -0300

    Create Segunda.java

---
+++ JavaPOO/Segunda.java
@@ -0,0 +1,13 @@
+package JavaPoo;
+
+public class Segunda {
+	public static void main (String[] args) {
+		Pessoa aluno1 = new Pessoa();
+		Pessoa aluno2 = new Pessoa();
+		aluno1.nome = "Heloise";
+		aluno2.nome = "Pietra";
+		System.out.println(aluno1.nome);
+		System.out.println(aluno2.nome);
+
+	}
+}

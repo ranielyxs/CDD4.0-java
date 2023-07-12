9999cb821d31da1fd63377e0dc148c55bfce65da
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 10:47:54 -0300

    Create ex1.java

---
+++ JavaPooMetodos/ex1.java
@@ -0,0 +1,17 @@
+package JavaPooMetodos;
+
+public class Metodos {
+	
+		int idade() {
+			return 20;
+		}
+		double valor() {
+			return 20.40;
+		}
+		String nome() {
+			return "Raniely";
+		}
+		boolean estado() {
+			return true;
+		}
+	}

1f1d2f7bde2213c7af3983a0974e9a92ac85c504
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 10:49:33 -0300

    Create ex4.java

---
+++ JavaPooMetodos/ex4.java
@@ -0,0 +1,13 @@
+package JavaPooMetodos;
+
+public class exSomar {
+	public static void main (String[] args) {
+		SomarMetodos objeto = new SomarMetodos();
+		int somar = objeto.somar(5, 7, 9);
+		
+		System.out.printf("somou tudo %d",somar);
+
+	}
+
+
+}

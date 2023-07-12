8b1f2452c1ff917955efbb02e0ed9864d69b2bbd
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-12 08:58:25 -0300

    Create Teste.java

---
+++ JavaPOO/Teste.java
@@ -0,0 +1,11 @@
+package JavaPoo;
+
+public class Teste {
+	public static void main (String[] args) {
+		Pessoa aluno1 = new Pessoa();
+		aluno1.nome = "Wellington";
+		System.out.println(aluno1.nome);
+		aluno1.comer();
+	}
+
+}

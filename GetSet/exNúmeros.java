dd7a4110666c0bc46d81e35f47d8fcdfd652d27a
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-17 09:15:13 -0300

    Create exNúmeros.java

---
+++ GetSet/exNúmeros.java
@@ -0,0 +1,15 @@
+package Math;
+
+public class exNúmeros {
+		public static void main (String[] args) {
+			Números pessoa = new Números();
+			pessoa.setN3(1);
+			pessoa.setN1(2);
+			pessoa.setN2(2003);
+			
+			System.out.println("A data é: \n"  + "Dia:" +pessoa.setN3(1) + "Mês:" + pessoa.setN1(2)+ "Ano:" + pessoa.setN2(2003));
+			
+
+		
+		}
+	}

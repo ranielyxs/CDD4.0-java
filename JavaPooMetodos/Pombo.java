3d5e6380013e083e13d9cba9ea2724d153a54668
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-17 11:41:05 -0300

    Create Pombo.java

---
+++ JavaPooMetodos/Pombo.java
@@ -0,0 +1,13 @@
+package JavaPooMetodos;
+
+public class Pombo extends Animal{
+		public Pombo(String nome, int idade) {
+			super(nome, idade);
+		}
+		public void Andar() {	
+			System.out.println("O Pombo andou");
+		}
+
+				
+
+}

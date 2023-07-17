0e8c0cb5d7f89f7dcd0ed1dc7c9b8421f50d3265
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-17 09:14:07 -0300

    Create Números.java

---
+++ GetSet/Números.java
@@ -0,0 +1,35 @@
+package Math;
+
+public class Números {
+	
+	private int n1;
+	private int n2;
+	private int n3;
+	private int n4;
+	
+	public int getN1() {
+		return n1;
+	}
+	public int setN1(int n1) {
+		return this.n1 = n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public int setN2(int n2) {
+		return this.n2 = n2;
+	}
+	public int getN3() {
+		return n3;
+	}
+	public int setN3(int n3) {
+		return this.n3 = n3;
+	}
+	public int getN4() {
+		return n4;
+	}
+	public int setN4(int n4) {
+		return this.n4 = n4;
+	}
+	
+	}

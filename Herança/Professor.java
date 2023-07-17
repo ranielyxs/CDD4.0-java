5c53f0fe210e97034dec81c1a550d643ea0214b4
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-07-17 09:12:10 -0300

    Create Professor.java

---
+++ Herança/Professor.java
@@ -0,0 +1,12 @@
+package Herança;
+import java.util.Date;
+
+  public class Professor extends Pessoa {
+    public Professor(String _nome, String _cpf, Date _data) {
+
+      super(_nome, _cpf, _data);
+      }
+      public double salario;
+      public String disciplina;
+      }
+
